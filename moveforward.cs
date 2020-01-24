6f704a789b27275eb926ce4ec4aeb15fc528463e
keesschaefers <56718135+keesschaefers@users.noreply.github.com>
2020-01-24 09:25:56 +0100

    Add files via upload

---
+++ moveforward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveforward : MonoBehaviour
+{
+    public float speed = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // zo kan de speler bewegen
+    void Update()
+    {
+        transform.Translate(Vector3.forward *Time.deltaTime * speed);
+    }
+}
---
+++ playercontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 20f;
+    public float xRange = 20f;
+
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // met deze code kan de speler niet van het veld af
+    void Update()
+    {
+        if ( transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if ( transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput =Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //vuurt een object af van de speler
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+    
+}
